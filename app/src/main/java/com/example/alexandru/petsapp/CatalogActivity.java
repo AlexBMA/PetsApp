f93c1fb1f7a619fb7b582b052fd8aa811fc8539e
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-05-12 00:02:40 +0300

    update 6

--- app/src/main/java/com/example/alexandru/petsapp/CatalogActivity.java
+++ app/src/main/java/com/example/alexandru/petsapp/CatalogActivity.java
@@ -34,7 +34,6 @@ public class CatalogActivity extends AppCompatActivity {
         displayDatabaseInfo();
     }
 
-
     private void displayDatabaseInfo() {
 
         PetDbHelper mDbHelper = new PetDbHelper(this);
