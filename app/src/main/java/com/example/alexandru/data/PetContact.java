bb30ed7a730ec627253b27b09e0b6c3b642258ab
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-05-20 17:31:34 +0300

    update 12

--- app/src/main/java/com/example/alexandru/data/PetContact.java
+++ app/src/main/java/com/example/alexandru/data/PetContact.java
@@ -1,5 +1,6 @@
 package com.example.alexandru.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,6 +9,9 @@ import android.provider.BaseColumns;
 
 public final class PetContact {
 
+    public static final String CONTENT_AUTHORITY = "com.example.alexandru.data.PetProvider";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
 
     public static abstract class PetEntry implements BaseColumns {
 
@@ -25,6 +29,8 @@ public final class PetContact {
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
 
         private static final String CREATE_TABLE = "";
