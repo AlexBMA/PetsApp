8182a02cab79087f9467f62ef6bfa3a6a950cbe1
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-05-21 16:03:46 +0300

    update 13

--- app/src/main/java/com/example/alexandru/data/PetProvider.java
+++ app/src/main/java/com/example/alexandru/data/PetProvider.java
@@ -2,6 +2,7 @@ package com.example.alexandru.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -17,6 +18,28 @@ public class PetProvider extends ContentProvider {
      * Tag for the log messages
      */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
+    private static final int PETS = 100;
+
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
+    private static final int PET_ID = 101;
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        sUriMatcher.addURI(PetContact.CONTENT_AUTHORITY, PetContact.PATH_PETS, PETS);
+        sUriMatcher.addURI(PetContact.CONTENT_AUTHORITY, PetContact.PATH_PETS + "/#", PET_ID);
+    }
 
     private PetDbHelper mDbHelper;
 
